9231eef3a9c411699e7ed5de4dff6ac42589b4e7
miguepoloc <miguepoloc@gmail.com>
2022-09-19 12:02:38 -0500

    Integración de lectura GPS con hardwareserial

--- ESP32_GPS/ESP32_GPS.ino
+++ ESP32_GPS/ESP32_GPS.ino
@@ -1,11 +1,8 @@
-// Librerías a utilizar
 #include <TinyGPS++.h>
 #include <SoftwareSerial.h>
 
 static const int RXPin = 3, TXPin = 1;
-//static const int RXPin = 16, TXPin = 17;
 static const uint32_t GPSBaud = 9600;
-int ledPin = 2;
 
 // The TinyGPS++ object
 TinyGPSPlus gps;
@@ -13,39 +10,34 @@ TinyGPSPlus gps;
 // The serial connection to the GPS device
 SoftwareSerial ss(RXPin, TXPin);
 
-char fecha[20] = "";
+char fecha[20] = " ";
 float latitud = 0, longitud = 0;
 unsigned long previousMillis = 0;        // will store last time LED was updated
 const long intervalo = 5000;
 
-void setup() {
+void setup(){
   Serial.begin(115200);
   ss.begin(GPSBaud);
-  pinMode (ledPin, OUTPUT);
 }
 
-void loop() {
-  // This sketch displays information every time a new sentence is correctly encoded.
+void loop(){
+  // Función para obtener los datos del GPS
   GPS();
-
+  
   unsigned long currentMillis = millis();
   if (currentMillis - previousMillis >= intervalo) {
     // save the last time you blinked the LED
     previousMillis = currentMillis;
-    Serial.print("Fecha= ");
-    Serial.print(fecha);
-    Serial.print(" Latitud= ");
-    Serial.print(latitud, 6);
-    Serial.print(" Longitud= ");
-    Serial.println(longitud, 6);
+    //Fecha*latitud*longitud
+    Serial.println(datosGPS());
   }
 }
 
+//Método que lee los datos provenientes del GPS y los guarda en las variables correspondientes
 void GPS() {
-  while (ss.available() > 0) {
+  while (ss.available() > 0){
     gps.encode(ss.read());
-    if (gps.location.isUpdated()) {
-      digitalWrite (ledPin, HIGH);
+    if (gps.location.isUpdated()){
       //Fecha y hora
       sprintf(fecha, "%02d-%02d-%02dT%02d:%02d:%02d", gps.date.year(), gps.date.month(),
               gps.date.day(), gps.time.hour(), gps.time.minute(), gps.time.second());
@@ -53,8 +45,10 @@ void GPS() {
       latitud = gps.location.lat();
       longitud = gps.location.lng();
     }
-    else {
-      digitalWrite (ledPin, LOW);
-    }
   }
+}
+
+//Método que devuelve la cadena de caracteres correspondiente a los datos de fecha*latitud*longitud
+String datosGPS() {
+  return String(fecha) + "*" + String(latitud,6) + "*" + String(longitud,6);
 }
---
+++ Test_GPS/Test_GPS.ino
@@ -0,0 +1,51 @@
+/*some obs:
+ *  * There are three serial ports on the ESP known as U0UXD, U1UXD and U2UXD.
+
+   U0UXD is used to communicate with the ESP32 for programming and during reset/boot.
+   U1UXD is unused and can be used for your projects. Some boards use this port for SPI Flash access though
+   U2UXD is unused and can be used for your projects.
+   on the code, i use the u2uxd. Dont forget to invert the rx/tx with the sensor ;)
+*/
+
+#include <HardwareSerial.h>
+#include <TinyGPS++.h>
+
+#define RXD2 16
+#define TXD2 17
+
+char fecha[20] = " ";
+float latitud = 0, longitud = 0;
+TinyGPSPlus gps;
+
+void setup() {
+  Serial.begin(115200);
+  Serial1.begin(9600, SERIAL_8N1, RXD2, TXD2); //gps baud
+}
+
+void loop() {
+  bool recebido = false;
+  while (Serial1.available()) {
+    char cIn = Serial1.read();
+    recebido = gps.encode(cIn);
+  }
+  if (gps.location.isUpdated() && gps.altitude.isUpdated())
+  {
+    Serial.print("D/M/A: ");
+    Serial.print(gps.date.value());
+    Serial.print(" | alt: ");
+    Serial.print(gps.altitude.feet());
+    Serial.print(" | satellites: ");
+    Serial.println(gps.satellites.value());
+    Serial.print(" | Latitud: ");
+    latitud = gps.location.lat();
+      longitud = gps.location.lng();
+    Serial.println(String(latitud,6));
+    Serial.print(" | Longitud: ");
+    Serial.println(String(longitud,6));
+    sprintf(fecha, "%02d-%02d-%02dT%02d:%02d:%02d", gps.date.year(), gps.date.month(),
+            gps.date.day(), gps.time.hour(), gps.time.minute(), gps.time.second());
+    Serial.print(" | Fecha: ");
+    Serial.println(fecha);
+    delay(500);
+  }
+}
